01c5e7f5eb10bebb2bbbd1be6888e92ddc56472e
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-21 11:45:43 -0800

    Add form to comments#index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,24 @@
 class CommentsController < ApplicationController
 
   def index
-  end
-
-  def new
+    @comments = Comment.all
   end
 
   def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to comments_path
+    else
+      flash[:note] = 'Return to sender, address unknown. No such number, no such zone.'
+      render 'form'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment,
+                                    :user)
   end
 
 end
